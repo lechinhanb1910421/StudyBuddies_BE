873ec3c79aa5b6f699b7aa54df32df70ac5bc089
Nhan Le <lechinhan012@gmail.com>
2023-02-27 20:11:14 +0700

    Add SQL scripts for majors

---
+++ src/main/resources/db/migration/release_1/V1_4__create_majors_table copy.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS PUBLIC.Majors (
+    majorId BIGSERIAL NOT NULL,
+    majorName VARCHAR(100) NOT NULL,
+    majorDescription TEXT NOT NULL,
+    followers BIGINT NOT NULL,
+    primary key (majorId)
+);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('General', 'Posts that are related to all majors', 272);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('Information Technology', 'Posts that are related to Information Technology major', 272);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('Information System', 'Posts that are related to Information System major', 272);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('Computer Science', 'Posts that are related to Computer Science major', 272);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('Software Engineering', 'Posts that are related to Software Engineering major', 272);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('Computer Network and Data Communication', 'Posts that are related to Computer Network and Data Communication major', 272);
+
+INSERT INTO PUBLIC.Majors("majorname", "majordescription", "followers")
+VALUES ('Multimedia Communication', 'Posts that are related to Multimedia Communication major', 272);
+
+SELECT * FROM PUBLIC.Majors;
