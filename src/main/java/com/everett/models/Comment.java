3a9a110474b0b81b22718dbf420ff555ea524a00
Nhan Le <lechinhan012@gmail.com>
2023-03-07 19:37:43 +0700

    Add Comment model

---
+++ src/main/java/com/everett/models/Comment.java
@@ -0,0 +1,108 @@
+package com.everett.models;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity(name = "Comments")
+@Table(name = "Comments", schema = "PUBLIC")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "commentId", nullable = false)
+    private Long commentId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "postId", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @Column(name = "createdTime", nullable = false)
+    private Timestamp createdTime;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public Comment() {
+    }
+
+    public Comment(Post post, User user, Timestamp createdTime, String content) {
+        this.post = post;
+        this.user = user;
+        this.createdTime = createdTime;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "[Comment no.: " + commentId + " Content: " + content + "postId: " + post.getPostId() + "userId: "
+                + user.getUserId() + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(commentId, comment.commentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(commentId);
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Timestamp getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Timestamp createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
