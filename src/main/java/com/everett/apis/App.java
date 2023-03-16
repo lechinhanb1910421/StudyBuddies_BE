ff7a040ac2170f2ea52722fb443af73462931123
Nhan Le <lechinhan012@gmail.com>
2023-03-16 21:13:45 +0700

    Add MP-JWT config

--- src/main/java/com/everett/apis/App.java
+++ src/main/java/com/everett/apis/App.java
@@ -1,9 +1,14 @@
 package com.everett.apis;
 
+import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.eclipse.microprofile.auth.LoginConfig;
+
 @ApplicationPath("/api")
+@LoginConfig(authMethod = "MP-JWT", realmName = "nlcntt")
+@DeclareRoles({ "admin", "visitor" })
 public class App extends Application {
 
 }
